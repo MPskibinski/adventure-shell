857d806b3520686ada8855a724d9f54a0796b4e5
MPskibinski <MarcinSk@24dcaea7d349>
2020-03-28 22:53:02 +0000

    lambda function in object condition

--- adventure-shell/main.py
+++ adventure-shell/main.py
@@ -6,42 +6,71 @@ from enum import Enum
 DISTANCE = Enum('DISTANCE','Self Held HeldContained Location Here HereContained Overthere NotHere UnknownObject')
 
 class GameObject:
-    def __init__(self, name,description,tag,location=None):
+    def __init__(self, name,description,tag,location=None,condition=None,false_description=''):
         self.name = name
-        self.description = description
+        self.__description = description
         self.tag = tag
         self.location = location
+        self.__false_description = false_description
+        if condition == None :
+            self.__lambda = lambda self: True
+        else:
+            self.__lambda = condition
+        
+    def condition(self):
+        return self.__lambda(self)
     
+    #@property
+    #def location(self):
+    #    return self.__location
+    
+    @property
+    def description(self):
+        if self.condition() : return self.__description
+        return self.__false_description
+
     def __repr__(self):
         return self.name
 
 class Road(GameObject):
-    def __init__(self, name, description, tag, location, destination):
-        super().__init__(name,description,tag,location)
-        self.destination = destination
+    def __init__(self, name, description, tag, location, destination,is_open='True',condition=None,false_description=''):
+        super().__init__(name,description,tag,location,condition,false_description)
+        self.__destination = destination
+        self.is_open = is_open
 
-player = GameObject('Ego','Yep..this is what we look like','player',1)
+    @property
+    def destination(self):
+        if self.condition() : return self.__destination
+        return None
+
+
+
+player = GameObject('Ego','Yep..this is what we look like',['player','self','me','us','ego'],1)
 
 game_objects =[
         player,
-        GameObject('Entrance to Guardroom','Abandoned guardroom, usually guarded by undead, constructs, or other creatures that do not need to eat or sleep','guardroom',None),
-        GameObject('Old Chamber','Dripping chamber inscribed with runes','chamber',None),
-        GameObject('Gold Key','Very old key. Probably opens door or chest','key',1),
-        GameObject('Wooden Chest','Sturdy chest made of red wood. Unfortunately it is closed','chest',2),
-        GameObject('Old Sword','Father\'s old rusty sword','sword',0),
-        GameObject('Storage','Storage, stocked with tools for maintaining the tomb and preparing the dead for burial','storage',None),
-        GameObject('Workshop','Workshop for embalming the dead','workshop',None),
-        GameObject('Skeleton','Good and cheap labor worker','skeleton',7),
-        Road('Stone stairs', 'Old slippery stone stairs', 'stairs',1,2),
-        Road('Chamber exit', 'Exit portal  made of stone blocks', 'exit',2,1),
-        Road('Wooden doors', 'Old crooked doors. Open','doors',2,6),
-        Road('Chamber entrance', 'Exit back to chamber','entrance',6,2)
+        GameObject('Entrance to Guardroom','Abandoned guardroom, usually guarded by undead, constructs, or other creatures that do not need to eat or sleep',['guardroom','entrance to guard room'],None),
+        GameObject('Old Chamber','Dripping chamber inscribed with runes',['chamber','old chamber'],None),
+        GameObject('Gold Key','Very old key. Probably opens door or chest',['key','gold key','old key'],1),
+        GameObject('Wooden Chest','Sturdy chest made of red wood. Unfortunately it is closed',['chest','wooden chest'],2),
+        GameObject('Old Sword','Father\'s old rusty sword',['sword','old sword','father\'s sword'],0),
+        GameObject('Storage','Storage, stocked with tools for maintaining the tomb and preparing the dead for burial',['storage'],None),
+        GameObject('Workshop','Workshop for embalming the dead',['workshop'],None),
+        GameObject('Skeleton','Good and cheap labor worker',['skeleton'],6),
+        Road('Stone stairs', 'Old slippery stone stairs', ['north','stairs','stone stairs'],1,2),
+        Road('Chamber exit', 'Exit portal  made of stone blocks', ['east','exit','chamber exit'],2,1),
+        Road('Wooden doors', 'Old crooked doors. Open',['west','doors','wooden doors'],2,6,False,lambda self : self.is_open,'Closed crooked doors'),
+        Road('Chamber entrance', 'Exit back to chamber',['east','entrance','chamber entrance'],6,2)
         ]
 
 def slowPrint(text,speed):
     for x in text:
         print(x, end='', flush=True)
         sleep(speed)
+
+def lookAt(obj):
+    print('You look at:')
+    slowPrint('***'+obj.name+'***\n'+obj.description+'\n',0.01)
 
 def render():
     screen=''
@@ -69,10 +98,9 @@ def getDistance(obj_from, obj_to):
 
 def findGameObjectByTag(tag, obj_to, max_distance):
     for obj in game_objects:
-        if obj.tag.lower()==tag.lower() and getDistance(obj,obj_to).value <= max_distance.value: return obj
+        if tag.lower() in [el.lower() for el in obj.tag]  and getDistance(obj,obj_to).value <= max_distance.value: return obj
     else: 
             return None
-
 
 def listObjectsAtLocation(location):
     return [obj for obj in game_objects if obj.location==location and obj!=player ]
@@ -116,6 +144,16 @@ def getRoad(location,destination):
                 return road
     return None
 
+def executeOpen(tag):
+    obj = getAccessibleObject(tag)
+    if obj != None:
+        get_distance = getDistance(player,obj)
+        if isinstance(obj,Road):
+            obj.is_open = True
+            print(tag,'is now open')
+
+    return False
+
 def executeGo(tag):
     go_target = getAccessibleObject(tag)
     get_distance = getDistance(player,go_target)
@@ -127,15 +165,15 @@ def executeGo(tag):
         return False
     if get_distance == DISTANCE.UnknownObject :
         return False
-#    if player.location == game_objects.index(go_target):
-#        print('I think we are already in place called',tag)
-#        return False
+    if get_distance == DISTANCE.Location :  
+        print('I think we are already in place called',tag)
+        return False
     if isinstance(go_target,Road):
+        if go_target.destination == None : 
+            print('This path is blocked')
+            return False
         player.location = go_target.destination
         return render
-#    if getRoad(player.location,game_objects.index(go_target)) != None:
-#        player.location = game_objects.index(go_target)
-#        return render
     print('We can\'t')
     
     return False
@@ -160,13 +198,17 @@ def executeGet(tag):
     
     return moveGameObject(obj,player) 
 
+
 def findNPC():
     for npc in game_objects: 
         if (npc.location == player.location and npc.tag == 'skeleton'): return npc
     else:
         return None
     
-
+def executeLookAt(tag):
+    obj = getAccessibleObject(tag)
+    if obj == None : return
+    lookAt(obj)
 
 def executeDrop(tag):
     return moveGameObject(getPossession(player,tag),game_objects[player.location])
@@ -183,6 +225,10 @@ class AdventureShell(Cmd):
     def do_look(self,line):
         if line == 'around':
             self.game_state = render()
+        elif len(line.split())>1:
+            cmd = line.split()
+            if cmd[0] == 'at' : 
+                executeLookAt(" ".join(el for el in cmd[1:]))
 
     def do_inv(self,line):
         self.game_state = executeInventory()
@@ -192,6 +238,9 @@ class AdventureShell(Cmd):
 
     def do_get(self,line):
         self.game_state = executeGet(line)
+    
+    def do_open(self,line):
+        self.game_state = executeOpen(line)
 
     def do_exit(self,line):
         return True
@@ -200,6 +249,5 @@ class AdventureShell(Cmd):
         if (stop == False or stop == None) and self.game_state:
             self.game_state()
         return stop
-
 os.system('clear')
 AdventureShell().cmdloop()
